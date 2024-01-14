98b41fc06a5fea0364d07d944e9e5a151d72c938
Dot-Link <150575884+Dot-Link@users.noreply.github.com>
2024-01-14 19:23:48 +0800

    Update pop.js

--- assests/css/js/pop.js
+++ assests/css/js/pop.js
@@ -1,51 +1,51 @@
-document.addEventListener("DOMContentLoaded", function () {
-    var modal = new bootstrap.Modal(document.getElementById('popupAd'));
+// //document.addEventListener("DOMContentLoaded", function () {
+//     var modal = new bootstrap.Modal(document.getElementById('popupAd'));
 
-    function getRandomAdURL() {
-        var adURLs = [
-            "https://ade409bb2fqjtza2jfc056pczf.hop.clickbank.net",
-            "https://www.fiverr.com/s/1xR7ak",
-            "https://www.fiverr.com/s/rzwo2x",
-            "https://shope.ee/VgICf1nNa",
-            "https://shope.ee/8pLQ84gZV8",
-            "https://shope.ee/508hZ2v2Hd",
-            "https://shope.ee/8KP9XCPs2N",
-            "https://shope.ee/2VRMaVKfIn",
-            "https://shope.ee/3fdJyfT9lN",
-            "https://shope.ee/AUTe7FObF0",
-            "https://shope.ee/8A5jL01TMc",
-            "https://shope.ee/8KP9XK0dle",
-            "https://shope.ee/8UiZjfCKQX",
-            "https://shope.ee/601El5lhcx",
-            "https://shope.ee/99yGWwdVwI",
-            "https://shope.ee/8A5jL7yNWx",
-            "https://shope.ee/6fGvYOGssN",
-            "https://shope.ee/6paLkibqPB",
-            "https://shope.ee/6AKexVe7Ey",
-            "https://shope.ee/99yGX2WPqD",
-            "https://shope.ee/9pDxKHY0kS",
-            "https://shope.ee/VgID9ozwd",
-            "https://shope.ee/5AS7ln0oom",
-            "https://shope.ee/9euX84sHzm",
-            "https://shope.ee/10cYoB2TZw",
-            // Add more 
-        ];
+//     function getRandomAdURL() {
+//         var adURLs = [
+//             "https://ade409bb2fqjtza2jfc056pczf.hop.clickbank.net",
+//             "https://www.fiverr.com/s/1xR7ak",
+//             "https://www.fiverr.com/s/rzwo2x",
+//             "https://shope.ee/VgICf1nNa",
+//             "https://shope.ee/8pLQ84gZV8",
+//             "https://shope.ee/508hZ2v2Hd",
+//             "https://shope.ee/8KP9XCPs2N",
+//             "https://shope.ee/2VRMaVKfIn",
+//             "https://shope.ee/3fdJyfT9lN",
+//             "https://shope.ee/AUTe7FObF0",
+//             "https://shope.ee/8A5jL01TMc",
+//             "https://shope.ee/8KP9XK0dle",
+//             "https://shope.ee/8UiZjfCKQX",
+//             "https://shope.ee/601El5lhcx",
+//             "https://shope.ee/99yGWwdVwI",
+//             "https://shope.ee/8A5jL7yNWx",
+//             "https://shope.ee/6fGvYOGssN",
+//             "https://shope.ee/6paLkibqPB",
+//             "https://shope.ee/6AKexVe7Ey",
+//             "https://shope.ee/99yGX2WPqD",
+//             "https://shope.ee/9pDxKHY0kS",
+//             "https://shope.ee/VgID9ozwd",
+//             "https://shope.ee/5AS7ln0oom",
+//             "https://shope.ee/9euX84sHzm",
+//             "https://shope.ee/10cYoB2TZw",
+//             // Add more 
+//         ];
 
-        return adURLs[Math.floor(Math.random() * adURLs.length)];
-    }
+//         return adURLs[Math.floor(Math.random() * adURLs.length)];
+//     }
 
-    modal.show();
+//     modal.show();
 
-    document.getElementById('popupCloseBtn').addEventListener('click', function () {
-        window.open(getRandomAdURL(), '_blank');
-    });
-    document.getElementById('pop-link').addEventListener('click', function () {
-        window.open(getRandomAdURL(), '_blank');
-    });
-    document.getElementById('popupCloseBtndown').addEventListener('click', function () {
-        window.open(getRandomAdURL(), '_blank');
-    });
+//     document.getElementById('popupCloseBtn').addEventListener('click', function () {
+//         window.open(getRandomAdURL(), '_blank');
+//     });
+//     document.getElementById('pop-link').addEventListener('click', function () {
+//         window.open(getRandomAdURL(), '_blank');
+//     });
+//     document.getElementById('popupCloseBtndown').addEventListener('click', function () {
+//         window.open(getRandomAdURL(), '_blank');
+//     });
 
-    modal._element.addEventListener('hidden.bs.modal', function () {
-    });
-});
+//     modal._element.addEventListener('hidden.bs.modal', function () {
+//     });
+// });
